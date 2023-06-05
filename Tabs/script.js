59ad54f04d2aced40a00f0ff1e0c378b68850be4
Vkscoma <vkscoma@gmail.com>
2023-06-05 15:37:45 -0700

    finishing touches

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,1 +1,24 @@
-console.log("it worked!")
+const tabContent = document.querySelectorAll('.tab--content');
+const tabButton = document.querySelectorAll('.tab--button');
+
+function tabClick(event) {
+    tabButton.forEach((button) => {
+        button.classList.remove('active');
+        button.classList.remove('background--match');
+    }
+    );
+    tabContent.forEach((content) => {
+        content.classList.remove('active');
+    }
+    );
+    for (let i = 0; i < tabButton.length; i++) {
+        if (tabButton[i] === event.currentTarget) {
+            tabButton[i].classList.add('background--match');
+            tabContent[i].classList.add('active');
+        }
+    }
+}
+
+tabButton.forEach((button) => {
+    button.addEventListener('click', tabClick);
+});
